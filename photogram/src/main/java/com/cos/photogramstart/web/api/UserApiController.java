dcac8ee1489ee514c63047594e7bd8623abe54ad
ju <heeju0805@gmail.com>
2024-06-15 22:48:10 +0900

    gitignore

--- photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
