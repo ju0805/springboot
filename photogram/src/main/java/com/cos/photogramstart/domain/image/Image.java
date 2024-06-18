b14b7ba494911fe64c3042497d4f53cefc764068
ju <heeju0805@gmail.com>
2024-06-18 13:06:33 +0900

    image upload 구현

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -0,0 +1,48 @@
+package com.cos.photogramstart.domain.image;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Image {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호 자동 증가(DB를 따라감) 
+	private int id;
+	
+	private String caption;
+	
+	private String postImageUrl;
+	
+	//이미지 좋아요 정보 
+	//이미지 댓글 정보 
+	
+	@JoinColumn(name="userId")
+	@ManyToOne
+	private User user;
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}
---
+++ photogram/src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
@@ -0,0 +1,14 @@
+package com.cos.photogramstart.web.dto.image;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class ImageUploadDto {
+
+	private MultipartFile file;
+	
+	private String caption;
+	
+}
